dc919b6fc8153a14fdb791f76ba07bb5d56ccc08
gos1004 <nnv711@gmail.com>
2021-03-21 23:25:14 +0900

    fixed untracked files

---
+++ rest-ap-with-spring/src/main/java/com/example/demo/RestApWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApWithSpringApplication.class, args);
+	}
+
+}
