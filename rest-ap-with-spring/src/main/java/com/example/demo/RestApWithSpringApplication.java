b523d0c018ad7837962d98dddadc492ec8f1a83d
gos1004 <nnv711@gmail.com>
2021-03-22 00:36:07 +0900

    Event 생성 및 TEST case 작성

--- rest-ap-with-spring/src/main/java/com/example/demo/RestApWithSpringApplication.java
+++ rest-ap-with-spring/src/main/java/com/example/demo/RestApWithSpringApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class RestApWithSpringApplication {
 
 	public static void main(String[] args) {
---
+++ rest-ap-with-spring/src/main/java/com/example/demo/events/EventStatus.java
@@ -0,0 +1,7 @@
+package com.example.demo.events;
+
+public enum EventStatus {
+
+    DRAFT, PUBLISHED, BEGAN_ENROLLMEND, CLOSED_ENROLLMENT, STARTED, ENDED
+
+}
