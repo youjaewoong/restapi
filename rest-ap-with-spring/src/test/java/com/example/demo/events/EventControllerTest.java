a2f61c5411c5a6d1a62682b11a39da50d0b42e3f
gos1004 <nnv711@gmail.com>
2021-03-28 02:12:41 +0900

    Event 생성 API 구현: 입력값 이외에 에러 발생

--- rest-ap-with-spring/src/test/java/com/example/demo/events/EventControllerTest.java
+++ rest-ap-with-spring/src/test/java/com/example/demo/events/EventControllerTest.java
@@ -10,24 +10,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.time.LocalDateTime;
 
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.hamcrest.Matchers;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 @RunWith(SpringRunner.class)
@@ -44,11 +38,41 @@ class EventControllerTest {
 	
 	//@MockBean
 	//EventRepository eventRepository;
-
     
 	@Test
 	void createEvent() throws Exception {
-		Event event = Event.builder()
+		EventDto event = EventDto.builder() //입력값 테스트
+				.name("Spring")
+				.description("REST API Development with Spring")
+				.beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23, 14, 21))
+				.closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 24, 14, 21))
+				.beginEventDateTime(LocalDateTime.of(2018, 11, 25, 14, 21))
+				.endEventDateTime(LocalDateTime.of(2018, 11, 26, 14, 21))
+				.basePrice(100)
+				.maxPrice(200)
+				.limitOfEnrollment(100)
+				.location("강남역 D2 스타텁 팩토리")
+				.build();
+		//event.setId(10);
+		//Mockito.when(eventRepository.save(event)).thenReturn(event);
+		
+		mockMvc.perform(post("/api/events/")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.accept(MediaTypes.HAL_JSON)
+				.content(objectMapper.writeValueAsString(event)))
+		.andDo(print())
+		.andExpect(status().isCreated()) //201
+		.andExpect(jsonPath("id").exists())
+		.andExpect(header().exists(HttpHeaders.LOCATION))
+		.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+		.andExpect(jsonPath("id").value(Matchers.not(100)))
+		.andExpect(jsonPath("free").value(Matchers.not(true)))
+		.andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+	}
+	
+	@Test
+	void createEvent_Bad_Request() throws Exception {
+		Event event = Event.builder() //입력값 이외 데이터가 있을 시 bad request 발생
 				.id(100)
 				.name("Spring")
 				.description("REST API Development with Spring")
@@ -71,13 +95,8 @@ class EventControllerTest {
 				.contentType(MediaType.APPLICATION_JSON_UTF8)
 				.accept(MediaTypes.HAL_JSON)
 				.content(objectMapper.writeValueAsString(event)))
-		.andDo(print())
-		.andExpect(status().isCreated()) //201
-		.andExpect(jsonPath("id").exists())
-		.andExpect(header().exists(HttpHeaders.LOCATION))
-		.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-		.andExpect(jsonPath("id").value(Matchers.not(100)))
-		.andExpect(jsonPath("free").value(Matchers.not(true)))
-		.andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+				.andDo(print())
+				.andExpect(status().isBadRequest()) //201
+		;
 	}
 }
