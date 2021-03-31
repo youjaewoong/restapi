efefb20f22e9437eb3680502d015bc6fe1fba6f5
gos1004 <nnv711@gmail.com>
2021-03-31 23:36:05 +0900

    Event 목록 조회 API

--- rest-ap-with-spring/src/main/java/com/example/demo/events/EventController.java
+++ rest-ap-with-spring/src/main/java/com/example/demo/events/EventController.java
@@ -7,12 +7,16 @@ import java.net.URI;
 import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -62,6 +66,14 @@ public class EventController {
 		eventResource.add(new Link("/docs/ndex.html#resources-events-create").withRel("profile"));
 		
 		return ResponseEntity.created(createdUri).body(eventResource);
+	}
+	
+	@GetMapping
+	public ResponseEntity<?> queryEvents(Pageable pageable, PagedResourcesAssembler<Event> assembler){
+		Page<Event> page = this.eventRepository.findAll(pageable);
+		var pagedResources = assembler.toModel(page, e -> new EventResource(e));
+		pagedResources.add(new Link("/docs/ndex.html#resources-events-list").withRel("profile"));
+		return ResponseEntity.ok(pagedResources);
 	}
 	
 	private ResponseEntity badRequest(Errors errors) {
--- rest-ap-with-spring/src/test/java/com/example/demo/events/EventControllerTest.java
+++ rest-ap-with-spring/src/test/java/com/example/demo/events/EventControllerTest.java
@@ -2,6 +2,7 @@ package com.example.demo.events;
 
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -15,6 +16,7 @@ import static org.springframework.restdocs.headers.HeaderDocumentation.headerWit
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 
 import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -49,6 +51,9 @@ class EventControllerTest {
 	
 	@Autowired
 	ObjectMapper objectMapper;
+	
+	@Autowired
+	EventRepository eventRepository;
 	
 	@Test
 	@TestDescription("정상적으로 이벤트를 생성하는 테스트")
@@ -198,4 +203,41 @@ class EventControllerTest {
 			.andExpect(jsonPath("_links.index").exists())
 		;
 	}
+	
+	@Test
+	@TestDescription("30개의 이벤트를 10개씩 두번째 페이지 조회하기")
+	public void queryEvents() throws Exception {
+		// Given
+		IntStream.range(0, 30).forEach(i -> {
+			this.generateEvent(i);
+		});
+		
+		// When
+		this.mockMvc.perform(get("/api/events")
+				.param("page", "1")
+				.param("size", "10")
+				.param("sort", "name,DESC")
+			)
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("page").exists())
+			.andExpect(jsonPath("_embedded.eventList[0]._links.self").exists())
+			.andExpect(jsonPath("_links.self").exists())
+			.andExpect(jsonPath("_links.profile").exists())
+			.andDo(document("query-events"))
+		;
+		
+	}
+
+	private void generateEvent(int index) {
+		// TODO Auto-generated method stub
+		Event event = Event.builder()
+				.name("event " + index)
+				.description("test event")
+				.build();
+		this.eventRepository.save(event);
+	}
+
+
+	
 }
